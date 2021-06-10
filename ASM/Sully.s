9e64d301a6b0010741b3ccd47abd36d13b44a7fb
thierry <thberrid@student.42.fr>
2021-06-10 13:42:23 +0200

    fix -1 index case

--- ASM/Sully.s
+++ ASM/Sully.s
@@ -1,6 +1,6 @@
 section .data
 	start_index db 5
-	data db "section .data%1$c%2$cstart_index db %4$d%1$c%2$cdata db %5$c%3$s%5$c, 0%1$c%2$cfilename :%1$c%2$c%2$c.fmt db %5$c%6$cs%6$cd%6$cs%5$c, 0%1$c%2$c%2$c.prefix db %5$cSully_%5$c, 0%1$c%2$c%2$c.suffix db %5$c.s%5$c, 0%1$csection .text%1$c%2$cextern open%1$c%2$cextern close%1$c%2$cextern dprintf%1$c%2$cextern sprintf%1$c%2$cglobal _start%1$c_start :%1$c%2$cpush rbp%1$c%2$cmov rbp, rsp%1$c%2$csub rsp, 32%1$c%2$cpush rdi%2$c; for alignment%1$c%2$cpush 0x25%2$c; 6nth arg%1$c%2$cpush 0x22%2$c; 5nth arg%1$c_set_index :%1$c%2$cxor r12, r12%1$c%2$cmov r12b, [start_index]%1$c_loop :%1$c%2$ccmp r12, 0%1$c%2$cje _end%1$c%2$c; create filename%1$c%2$clea rdi, [rbp - 32]%1$c%2$cmov rsi, filename.fmt%1$c%2$cmov rdx, filename.prefix%1$c%2$cmov rcx, r12%1$c%2$cmov r8, filename.suffix%1$c%2$cxor rax, rax%1$c%2$ccall sprintf%1$c%2$c; open filename%1$c%2$clea rdi, [rbp - 32]%1$c%2$cmov rsi, 102%1$c%2$cmov edx, 511%1$c%2$cmov rax, 0%1$c%2$ccall open%1$c%2$c; save FD%1$c%2$cxor r13, r13%1$c%2$cmov r13, rax%1$c%2$c; write in file%1$c%2$cmov rdi, rax%1$c%2$cmov rsi, data%1$c%2$cmov rdx, 0x0a%1$c%2$cmov rcx, 0x09%1$c%2$cmov r8,  data%1$c%2$cmov r9,  r12%1$c%2$cmov rax, 2%1$c%2$ccall dprintf%1$c%2$c; close%1$c%2$cmov rdi, r13%1$c%2$ccall close%1$c%2$c; dec%1$c%2$csub r12, 1%1$c%2$cjmp _loop%1$c_end :%1$c%2$cmov rsp, rbp%1$c%2$cpop rbp%1$c%2$cmov rbx, 0%1$c%2$cmov rax, 1%1$c%2$cint 0x80", 0
+	data db "section .data%1$c%2$cstart_index db %4$d%1$c%2$cdata db %5$c%3$s%5$c, 0%1$c%2$cfilename :%1$c%2$c%2$c.fmt db %5$c%6$cs%6$cd%6$cs%5$c, 0%1$c%2$c%2$c.prefix db %5$cSully_%5$c, 0%1$c%2$c%2$c.suffix db %5$c.s%5$c, 0%1$csection .text%1$c%2$cextern open%1$c%2$cextern close%1$c%2$cextern dprintf%1$c%2$cextern sprintf%1$c%2$cglobal _start%1$c_start :%1$c%2$cpush rbp%1$c%2$cmov rbp, rsp%1$c%2$csub rsp, 32%1$c%2$cpush rdi%2$c; for alignment%1$c%2$cpush 0x25%2$c; 6nth arg%1$c%2$cpush 0x22%2$c; 5nth arg%1$c_set_index :%1$c%2$cxor r12, r12%1$c%2$cmov r12b, [start_index]%1$c_loop :%1$c%2$ccmp r12, 0%1$c%2$cjl _end%1$c%2$c; create filename%1$c%2$clea rdi, [rbp - 32]%1$c%2$cmov rsi, filename.fmt%1$c%2$cmov rdx, filename.prefix%1$c%2$cmov rcx, r12%1$c%2$cmov r8, filename.suffix%1$c%2$cxor rax, rax%1$c%2$ccall sprintf%1$c%2$c; open filename%1$c%2$clea rdi, [rbp - 32]%1$c%2$cmov rsi, 102%1$c%2$cmov edx, 511%1$c%2$cmov rax, 0%1$c%2$ccall open%1$c%2$c; save FD%1$c%2$cxor r13, r13%1$c%2$cmov r13, rax%1$c%2$c; write in file%1$c%2$cmov rdi, rax%1$c%2$cmov rsi, data%1$c%2$cmov rdx, 0x0a%1$c%2$cmov rcx, 0x09%1$c%2$cmov r8,  data%1$c%2$cmov r9,  r12%1$c%2$cmov rax, 2%1$c%2$ccall dprintf%1$c%2$c; close%1$c%2$cmov rdi, r13%1$c%2$ccall close%1$c%2$c; dec%1$c%2$csub r12, 1%1$c%2$cjmp _loop%1$c_end :%1$c%2$cmov rsp, rbp%1$c%2$cpop rbp%1$c%2$cmov rbx, 0%1$c%2$cmov rax, 1%1$c%2$cint 0x80", 0
 	filename :
 		.fmt db "%s%d%s", 0
 		.prefix db "Sully_", 0
@@ -23,7 +23,7 @@ _set_index :
 	mov r12b, [start_index]
 _loop :
 	cmp r12, 0
-	je _end
+	jl _end
 	; create filename
 	lea rdi, [rbp - 32]
 	mov rsi, filename.fmt
--- C/Sully.c
+++ C/Sully.c
@@ -7,9 +7,9 @@ int main (void)
 {
 	int i = 5;
 	char filename[19];
-	char *s = "#include <unistd.h>%1$c#include <stdio.h>%1$c#include <sys/types.h>%1$c#include <sys/stat.h>%1$c#include <fcntl.h>%1$cint main (void)%1$c{%1$c%2$cint i = %6$d;%1$c%2$cchar filename[19];%1$c%2$cchar *s = %3$c%5$s%3$c;%1$c%2$cint fd;%1$c%2$cwhile (i)%1$c%2$c{%1$c%2$c%2$csprintf(filename, %3$c%7$cs%7$cd%7$cs%3$c, %3$cSully_%3$c, i, %3$c.c%3$c);%1$c%2$c%2$cfd = open(filename, O_CREAT|O_RDWR, 0777);%1$c%2$c%2$cdprintf(fd, s, '%4$cn', '%4$ct', '%3$c', '%4$c%4$c', s, i, '%7$c');%1$c%2$c%2$cclose(fd);%1$c%2$c%2$ci -= 1;%1$c%2$c}%1$c%2$creturn (0);%1$c}";
+	char *s = "#include <unistd.h>%1$c#include <stdio.h>%1$c#include <sys/types.h>%1$c#include <sys/stat.h>%1$c#include <fcntl.h>%1$cint main (void)%1$c{%1$c%2$cint i = %6$d;%1$c%2$cchar filename[19];%1$c%2$cchar *s = %3$c%5$s%3$c;%1$c%2$cint fd;%1$c%2$cwhile (i >= 0)%1$c%2$c{%1$c%2$c%2$csprintf(filename, %3$c%7$cs%7$cd%7$cs%3$c, %3$cSully_%3$c, i, %3$c.c%3$c);%1$c%2$c%2$cfd = open(filename, O_CREAT|O_RDWR, 0777);%1$c%2$c%2$cdprintf(fd, s, '%4$cn', '%4$ct', '%3$c', '%4$c%4$c', s, i, '%7$c');%1$c%2$c%2$cclose(fd);%1$c%2$c%2$ci -= 1;%1$c%2$c}%1$c%2$creturn (0);%1$c}";
 	int fd;
-	while (i)
+	while (i >= 0)
 	{
 		sprintf(filename, "%s%d%s", "Sully_", i, ".c");
 		fd = open(filename, O_CREAT|O_RDWR, 0777);
